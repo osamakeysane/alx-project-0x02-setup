48baf728a31cad4780835d199ce28302d5a15f5c
mohamed keytsane <osamakeysane20@gmail.com>
2025-09-22 14:33:22 +0300

    add the interface for the button

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -2,3 +2,9 @@ export interface CardProps {
   title: string;
   content: string;
 }
+export interface ButtonProps {
+  size?: "small" | "medium" | "large"; // optional size prop
+  shape?: "rounded-sm" | "rounded-md" | "rounded-full"; // optional shape prop
+  children: React.ReactNode; // button text or content
+  onClick?: () => void; // optional click handler
+}
